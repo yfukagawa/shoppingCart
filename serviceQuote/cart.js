170a91209299e03fe5be5d47ee936fe2025288c1
yfukagawa <yfukagawa@rooseveltinnovations.com>
2024-05-11 12:33:32 -0400

    Initial commit

---
+++ serviceQuote/cart.js
@@ -0,0 +1,71 @@
+let eventIndex = 0;
+
+function wedding() {
+    eventIndex = 1;
+    var x = document.getElementById('weddingDetail');
+    if(x.style.visibility !== 'visible') {
+        x.style.visibility = 'visible';
+    } 
+    multiHide();
+    console.log('wedding', eventIndex)
+    
+}
+function branch() {
+    eventIndex = 2;
+    var x = document.getElementById('weddingDetail');
+    if(x.style.visibility !== 'hidden') {
+        x.style.visibility = 'hidden';
+    } 
+    multiHide();
+    console.log('branch', eventIndex)
+    
+}
+
+function dinner() {
+    eventIndex = 2;
+    var x = document.getElementById('weddingDetail');
+    if(x.style.visibility !== 'hidden') {
+        x.style.visibility = 'hidden';
+    } 
+    multiHide();
+    console.log('dinner', eventIndex)
+    
+}
+
+function party() {
+    eventIndex = 2;
+    var x = document.getElementById('weddingDetail');
+    if(x.style.visibility !== 'hidden') {
+        x.style.visibility = 'hidden';
+    } 
+    multiHide();
+    console.log('party', eventIndex)
+}
+
+function basicService() {
+    var x = document.getElementById('basicServiceInfo');
+    var y = document.getElementById('receptionOnlyInfo');
+    if(x.style.visibility !== 'visible') {
+        x.style.visibility = 'visible';
+    }
+    
+        y.style.visibility = 'hidden';
+}
+function receptionOnly() {
+    var y = document.getElementById('basicServiceInfo');
+    var x = document.getElementById('receptionOnlyInfo');
+    if(x.style.visibility !== 'visible') {
+        x.style.visibility = 'visible';
+    }
+    
+        y.style.visibility = 'hidden';
+
+}
+
+function multiHide() {
+    var y = document.getElementById('basicServiceInfo');
+    var z = document.getElementById('receptionOnlyInfo');
+    y.style.visibility = 'hidden';
+    z.style.visibility = 'hidden';
+}
+
